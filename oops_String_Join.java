9e6359de2fb301d41be7312c31f35f59d8982e4c
shaikharshad6730-stack <shaikharshad6730@gmail.com>
2026-03-04 20:36:32 +0530

    Create oops_String_Join.java

---
+++ oops_String_Join.java
@@ -0,0 +1,54 @@
+public class OOPSDisplayUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                " *****   ",
+                " *****   ",
+                " *****   ",
+                " ***** "
+        ));
+
+        System.out.println(String.join("",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     "
+        ));
+
+        System.out.println(String.join("",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     "
+        ));
+
+        System.out.println(String.join("",
+                "*     * ",
+                "*     * ",
+                " *****   ",
+                " ***** "
+        ));
+
+        System.out.println(String.join("",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "     * "
+        ));
+
+        System.out.println(String.join("",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "     * "
+        ));
+
+        System.out.println(String.join("",
+                " *****   ",
+                " *****   ",
+                "*       ",
+                " *****  "
+        ));
+    }
+}
